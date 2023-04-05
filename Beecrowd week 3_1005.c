fd283ca23f0f962bb7c12d58c7e148344614440d
maisha <124515279+Maisha-mp@users.noreply.github.com>
2023-04-06 00:30:36 +0600

    Update Beecrowd week 3_1005.c

--- Beecrowd week 3_1005.c
+++ Beecrowd week 3_1005.c
@@ -1,17 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
+ 
+int main() {
+ 
+   
+   double A, B, MEDIA;
 
-int main ()
+    scanf("%lf" "%lf", &A, &B);
 
-{
+    MEDIA= (A * 3.5+B * 7.5) / (3.5+7.5);
 
-    int A, B, C, D, DIFERENCA;
-
-    scanf("%d" "%d" "%d" "%d", &A, &B, &C, &D);
-
-
-    DIFERENCA= (A * B-C * D);
-
-    printf("DIFERENCA = %d\n", DIFERENCA);
-
+    printf("MEDIA = %.5lf\n", MEDIA);
+    
     return 0;
 }
