f21b09cfa564a4fab7e8841ae42efd4df8f4fc40
maisha <124515279+Maisha-mp@users.noreply.github.com>
2023-04-06 11:00:08 +0600

    Beecrowd week 2_2764

--- Beecrowd week 2_2764.c
+++ Beecrowd week 2_2764.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
-
+ 
 int main() {
-
-    int A,B,C;
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     
+     int A,B,C;
 
     scanf("%d/" "%d/" "%d", &A, &B, &C);
 
-    printf("%d/%d/%d\n", B, A, C);
+    printf("%.2d/%.2d/%.2d\n", B, A, C);
 
-    printf("%d/%d/%d\n", C, B, A);
+    printf("%.2d/%.2d/%.2d\n", C, B, A);
 
-    printf("%d-%d-%d\n", A, B, C);
-
-
+    printf("%.2d-%.2d-%.2d\n", A, B, C);
+ 
     return 0;
 }
